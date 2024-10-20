daeb8cf06a9dd81747cd26a20632d25481bf55ff
tanzim_safin <tanzimhossainsafin756@gmail.com>
2024-10-20 22:42:03 +0600

    add Context API without rapper component

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,41 @@
-import { useState } from "react";
+import { useState, createContext, useContext } from "react";
+const Bulbcontext = createContext();//create a context
 function App() {
-  const [light,setLight]=useState(true);
-  return(<div>
-     <Light light={light} setLight={setLight}/>
-     {/* least common ancestor */}
-  </div>);
+  const [light, setLight] = useState(true);
+  return (
+    <div>
+     <Bulbcontext.Provider value={{light:light,setLight:setLight}}>
+          <Light />
+     </Bulbcontext.Provider>{/* wrapped in inside a provider / */}
+      
+      {/* least common ancestor */}
+    </div>
+  );
 }
-function Light({light,setLight}){//we have to pass a extra props here which doesn't need this light setLight veriable at all .Rather it just passes the props to it decendent
-  
-  return (<div>
-    <LightBulb light={light}/>
-    <LightSwitch setLight={setLight}/>
-  </div>);
+function Light() {
+  //we have to pass a extra props here which doesn't need this light setLight veriable at all .Rather it just passes the props to it decendent
+
+  return (
+    <div>
+      <LightBulb />
+      <LightSwitch  />
+    </div>
+  );
 }
-function LightBulb({light}){
-  return (<div>
-    {light?"light is on":"light is off"}
-  </div>)
+function LightBulb() {
+  const {light}=useContext(Bulbcontext);//consume the context
+  return <div>{light ? "light is on" : "light is off"}</div>;
 }
-function LightSwitch({setLight}){
-  function togoleSwitch(){
-    setLight(light=>!light);
-  }; 
-    return(<div>
+function LightSwitch() {
+  const {setLight}=useContext(Bulbcontext);
+  function togoleSwitch() {
+    setLight((light) => !light);
+  }
+  return (
+    <div>
       <button onClick={togoleSwitch}>LightSwitch</button>
-    </div>)
-    
+    </div>
+  );
 }
 
-export default App
+export default App;
