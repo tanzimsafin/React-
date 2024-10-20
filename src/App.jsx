1564820a2dcbb9476207997b16bc579c39bcd489
tanzim_safin <tanzimhossainsafin756@gmail.com>
2024-10-20 22:15:36 +0600

    Normal way Props chain

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import { useState } from "react";
-
-
 function App() {
+  const [light,setLight]=useState(true);
   return(<div>
-     <Light/>
+     <Light light={light} setLight={setLight}/>
+     {/* least common ancestor */}
   </div>);
 }
-function Light(){
-  const [bulbState,SetbulbState]=useState(true);
-  const [light,setLight]=useState(true);
+function Light({light,setLight}){//we have to pass a extra props here which doesn't need this light setLight veriable at all .Rather it just passes the props to it decendent
+  
   return (<div>
     <LightBulb light={light}/>
     <LightSwitch setLight={setLight}/>
-  </div>)
+  </div>);
 }
 function LightBulb({light}){
   return (<div>
